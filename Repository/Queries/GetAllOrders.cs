2e4294e7668ac4444c2b5bb6cafb920347402d67
angielynr <106809120+angielynr@users.noreply.github.com>
2022-08-02 18:32:54 +0800

    View All Orders (#3)
    * Added repo, service, controller
    
    * Updated order controller response
    Updated service queries
    
    Co-authored-by: angielyn.rivera <angielyn.rivera@SVPHLP00674>

---
+++ Repository/Queries/GetAllOrders.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository.ConnectionHandler;
+using Repository.Interfaces;
+using Repository.RepositoryDTO;
+
+namespace Repository.Queries
+{
+    public partial class RepositoryQuery : IRepositoryQueries
+    {
+        public async Task<List<Orders>> GetAllOrders()
+        {
+            try
+            {
+                return await _dbContext.orders.ToListAsync();
+            }
+            catch
+            {
+                return null;
+            }
+            finally
+            {
+                CloseConnection.DisposeConnection();
+            }
+
+        }
+    }
+}
---
+++ Services/ServicesDTO/OrdersDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Services.ServicesDTO
+{
+    public class OrdersDTO
+    {
+        [Key]
+        public int order_id { get; set; }
+        public double order_amount { get; set; }
+        public int order_quantity { get; set; }
+        public int ordered_by { get; set; }
+        public string order_date { get; set; }
+    }
+}
