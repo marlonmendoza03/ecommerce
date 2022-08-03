282cd87b7b6a7336a0e3753ee00f799e756c931d
marlonmendoza03 <marlon.mendoza@softvision.com>
2022-08-03 14:06:53 +0800

    Transitioned Web-API project to MVC Project

---
+++ EcommerceMVC/EcommerceDTOs/OrdersResponseDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.EcommerceDTOs
+{
+    public class OrdersResponse
+    {
+        public List<OrdersResponseDTO> Orders { get; set; }
+    }
+    public class OrdersResponseDTO
+    {
+        [Key]
+        public int OrderId { get; set; }
+        public double OrderAmount { get; set; }
+        public int OrderQuanity { get; set; }
+        public int OrderedBy { get; set; }
+        public string OrderDate { get; set; }
+    }
+}
