f48aaa08a96a95eb8a41101c767b9523b3e92e15
marlonmendoza03 <marlon.mendoza@softvision.com>
2022-08-04 14:58:29 +0800

    Fixed some more namespace errors

--- EcommerceMVC/Controllers/ProductsController.cs
+++ EcommerceMVC/Controllers/ProductsController.cs
@@ -1,5 +1,5 @@
-﻿using Ecommerce.Exceptions;
-using EcommerceMVC.EcommerceDTOs;
+﻿using EcommerceMVC.EcommerceDTOs;
+using EcommerceMVC.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Services.Interfaces;
 using Services.ServicesDTO;
