9de1bbb89a515166df0efbb1d143368e4960e302
renrxx <72149979+renrxx@users.noreply.github.com>
2022-08-08 15:24:48 +0800

    Feature/Delete Endpoint final (#15)
    * Initail code for Delete product.
    
    * merged from main
    
    * Initial codes for delete. No response added yet.
    
    * Codes for delete endpoint
    
    * removed unnecessary code/usings
    
    * new pr
    
    * Merged from main
    
    * Final

--- Repository/Interfaces/IRepositoryQueries.cs
+++ Repository/Interfaces/IRepositoryQueries.cs
@@ -8,6 +8,7 @@ namespace Repository.Interfaces
         Task<List<Logs>> GetAllLogs();
         Task<List<Orders>> GetAllOrders();
         Task<List<Products>> GetAllProducts();
+        Task<ProductReponse> GetProductById(int id);
         Task<ProductDetails> GetProductwithName(string productName);
         Task<List<ProductDetails>> GetProductWithProductNameAndDesctiption(string? productName, string? productDescription);
     }
