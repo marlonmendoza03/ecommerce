430978d56399a39cbd23c1d67a34e421acfb0329
Reynard Ramirez <reynardramirez021@gmail.com>
2022-08-08 15:12:04 +0800

    Merged from main

--- EcommerceMVC/Exceptions/DeleteResponse.cs
+++ EcommerceMVC/Exceptions/DeleteResponse.cs
@@ -7,6 +7,6 @@ namespace EcommerceMVC.Exceptions
         [Key]
         public int ProductId { get; set; }
 
-        public string? Result { get; set; } = "Successfully Deleted";
+        public string? Result { get; set; } = "Success";
     }
 }
