07c4941ac0ba54f8c6a19af031e4103fa6c5fcec
marlonmendoza03 <marlon.mendoza@softvision.com>
2022-08-15 15:11:57 +0800

    Added admin controller

--- EcommerceMVC/Controllers/AdminController.cs
+++ EcommerceMVC/Controllers/AdminController.cs
@@ -2,19 +2,45 @@
 using Microsoft.AspNetCore.Mvc;
 using Services.Interfaces;
 
-namespace Ecommerce.Controllers
+namespace EcommerceMVC.Controllers
 {
-    [Route("orders")]
-    public class OrdersController : Controller
+    [Route("admin")]
+    public class AdminController : Controller
     {
         private readonly IServiceQueries _serviceQueries;
 
-        public OrdersController(IServiceQueries serviceQueries)
+        public AdminController(IServiceQueries serviceQueries)
         {
             _serviceQueries = serviceQueries;
         }
 
-        [HttpGet]
+        [HttpGet("logs")]
+        public async Task<IActionResult> Logs()
+        {
+            var response = new LogsResponseModel();
+            var logs = await _serviceQueries.GetAllLogs();
+            var logsResponse = new List<LogsModel>();
+            if (logs == null)
+            {
+                return null;
+            }
+
+            foreach (var logItems in logs)
+            {
+                logsResponse.Add(new LogsModel()
+                {
+                    LogId = logItems.LogId,
+                    LogDescription = logItems.LogDescription,
+                    LogDate = logItems.LogDate,
+                    AddedBy = logItems.AddedBy
+                });
+            }
+
+            response.Logs = logsResponse;
+            return View(response);
+        }
+
+        [HttpGet("orders")]
         public async Task<IActionResult> Orders()
         {
             var orders = await _serviceQueries.GetAllOrders();
